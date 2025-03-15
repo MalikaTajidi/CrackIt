dae7741f9e922476ee3a0f24d0f22f031b5c7c41
MalikaTajidi <tajidimalika@gmail.com>
2025-03-15 15:27:01 +0100

    Register component 0

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './features/landing-page/landing-page.component';
 import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent},
     { path: 'login', component: LoginComponent},
+    { path: 'register', component:RegisterComponent},
 ];
