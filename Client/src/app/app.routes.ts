2e8952cabed99fe93a60cff73aead132967b4cc6
MalikaTajidi <tajidimalika@gmail.com>
2025-03-16 18:09:28 +0100

    topics list styling

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './features/landing-page/landing-page.component';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { TopicComponent } from './features/topic/topic.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent},
     { path: 'login', component: LoginComponent},
     { path: 'register', component:RegisterComponent},
-    { path: 'navbar', component:NavbarComponent},
-    { path: 'footer', component:FooterComponent},
+    {path: 'topics',component:TopicComponent},
 ];
---
+++ Client/src/app/features/topic/topic.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-topic',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent,CommonModule],
+  templateUrl: './topic.component.html',
+  styleUrl: './topic.component.css'
+})
+export class TopicComponent {
+ // Dummy data for topics
+ topics = [
+  { id: 1, title: 'Mathematics', description: 'Explore the world of numbers, shapes, and patterns.' },
+  { id: 2, title: 'Science', description: 'Dive into the mysteries of the natural world and scientific discoveries.' },
+  { id: 3, title: 'History', description: 'Uncover the past events that shaped the modern world.' },
+  { id: 4, title: 'Literature', description: 'Discover the beauty of written works and storytelling from various cultures.' },
+  { id: 5, title: 'Mathematics', description: 'Learn advanced concepts and techniques in the realm of mathematics.' },
+  { id: 6, title: 'Science', description: 'Study the fundamental principles and laws governing the physical world.' },
+  { id: 7, title: 'History', description: 'Explore ancient civilizations and their impact on modern society.' },
+  { id: 8, title: 'Literature', description: 'Analyze classic and contemporary literary works and their societal influences.' }
+];
+
+topic: any;
+
+constructor(private router: Router) {}
+
+// Navigate to SubtopicComponent upon selecting a topic
+onTopicSelect(topicId: number) {
+  this.router.navigate(['/subtopics', topicId]);  // Update this route based on your actual routing configuration
+}
+}
