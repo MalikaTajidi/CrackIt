8c647c89a1479652dfc9a879bc07a4f34ebc5239
MalikaTajidi <tajidimalika@gmail.com>
2025-03-16 17:15:02 +0100

    footer styling

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingPageComponent } from './features/landing-page/landing-page.compo
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent},
     { path: 'login', component: LoginComponent},
     { path: 'register', component:RegisterComponent},
     { path: 'navbar', component:NavbarComponent},
+    { path: 'footer', component:FooterComponent},
 ];
