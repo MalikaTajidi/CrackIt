ad77b68143b675470da69c82a86c59ba16aa34ac
MalikaTajidi <tajidimalika@gmail.com>
2025-03-29 13:51:23 +0100

    we are baaaack!

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { TopicComponent } from './features/topic/topic.component';
 import { QuestionsComponent } from './features/questions/questions.component';
+import { QuestionComponent } from './features/question/question.component';
 
 
 export const routes: Routes = [
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'register', component:RegisterComponent},
     {path: 'topics',component:TopicComponent},
     {path: 'questions',component:QuestionsComponent},
+    {path: 'question',component:QuestionComponent},
     
 ];
--- Client/src/app/features/question/question.component.ts
+++ Client/src/app/features/question/question.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './question.component.html',
   styleUrl: './question.component.css'
 })
 export class QuestionComponent {
+  question: string = "What is the capital of France?";
+  options: string[] = ["Berlin", "Madrid", "Paris", "Rome"];
+  selectedOption: string | null = null;
 
+  selectOption(option: string) {
+    this.selectedOption = option;
+  }
 }
