0fc145cd398e771fd3be5aff2d8a499d5a21d1b6
MalikaTajidi <tajidimalika@gmail.com>
2025-04-07 16:42:59 +0200

    this is taking foreveeeer!

--- Client/src/app/features/profile/profile.component.ts
+++ Client/src/app/features/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
